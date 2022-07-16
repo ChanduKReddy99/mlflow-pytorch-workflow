6d1dff2981b8379d872f8881d0db9022800ee174
ChanduKReddy99 <chanduk.amical@gmail.com>
2022-07-16 10:11:52 +0200

    stage02 getting data completed with mlflow logs

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -31,3 +31,8 @@ class ConvNet(nn.Module):
 def save_binary(model_config, filepath):
     joblib.dump(model_config, filepath)
     logging.info(f"model_config is saved at {filepath}")
+
+def load_binary(filepath):
+    bin_file = joblib.load(filepath)
+    logging.info(f"model_config is loaded from {filepath}")
+    return bin_file
